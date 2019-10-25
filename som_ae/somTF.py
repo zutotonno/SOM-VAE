76130589a47955f55af414a6e21c8d35d293a211
zutotonno <ritacco.ant@gmail.com>
2019-10-25 19:15:53 +0200

    som now works. I will move it  also to another repo

--- som_ae/somTF.py
+++ som_ae/somTF.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
 import functools
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+# import io
+# import itertools
+# import numpy as np
+
 
 def lazy_scope(function):
     """Creates a decorator for methods that makes their return values load lazily.
@@ -28,7 +35,7 @@ def lazy_scope(function):
 
 class somTF:
     def __init__(self, inputs=None, latent_dim=None , som_dim=None, learning_rate=None,
-                 decay_factor=None, decay_steps=None, batch_size = None):
+                 decay_factor=None, decay_steps=None, batch_size = None, alpha = None):
         self.inputs = inputs
         self.som_dim = som_dim
         self.latent_dim = latent_dim
@@ -36,19 +43,29 @@ class somTF:
         self.decay_factor = decay_factor
         self.decay_steps = decay_steps
         self.batch_size = batch_size
+        self.alpha = alpha
 
         self.codebook
         self.global_step
-        self.BMU
+        self.k_index
+        # self.loss_bmu
+        # self.loss_neigh
+        self.loss_bmu
+        self.loss_bmu_up
+        self.loss_bmu_bottom
+        self.loss_bmu_right
+        self.loss_bmu_left
         self.loss
         self.optimize
+
+        # self.plot_neurons
 
     @lazy_scope
     def codebook(self):
         """Creates variable for the SOM embeddings."""
         embeddings = tf.get_variable("codebook", [self.som_dim[0]*self.som_dim[1] , self.latent_dim],
                                      initializer=tf.truncated_normal_initializer(stddev=0.05))
-        tf.summary.tensor_summary("codebook", embeddings)
+        tf.compat.v1.summary.tensor_summary("codebook", embeddings)
         return embeddings
 
     @lazy_scope
@@ -58,83 +75,167 @@ class somTF:
         return global_step
 
     @lazy_scope
-    def BMU(self):
-        squared_distances = tf.squared_difference(tf.expand_dims(self.inputs, 1),
+    def k_index(self):
+        squared_distances = tf.math.squared_difference(tf.expand_dims(self.inputs, 1),
                                                   tf.expand_dims(self.codebook, 0))
-        component_sum = tf.reduce_sum(squared_distances, axis=-1)
-        min_idx = tf.argmin(component_sum, -1)
+        component_sum = tf.reduce_sum(input_tensor=squared_distances, axis=-1)
+        min_idx = tf.argmin(input=component_sum, axis=-1)
+        k_0 = min_idx // self.som_dim[0]
+        k_1 = min_idx % self.som_dim[1]
+        tf.compat.v1.summary.histogram("bmu_0", k_0)
+        tf.compat.v1.summary.histogram("bmu_1", k_1)
         return min_idx
 
     @lazy_scope
-    def neighbors(self):
-        min_idx = self.BMU
-        # min_idx_reshaped_0 = min_idx // self.som_dim[0]
-        # min_idx_reshaped_1 = min_idx % self.som_dim[1]
-        # min_idx_reshaped = tf.stack([min_idx_reshaped_0, min_idx_reshaped_1], axis=1)
-        # min_idx_reshaped = tf.dtypes.cast(min_idx_reshaped, tf.int32)
-        # up_idx = tf.cond(min_idx_reshaped[0] > tf.constant(0), lambda: tf.add(min_idx_reshaped, tf.constant([-1, 0])),
-        #                  lambda: min_idx_reshaped)
-        # bottom_idx = tf.cond(min_idx_reshaped[0] < self.som_dim[0],
-        #                      lambda: tf.add(min_idx_reshaped, tf.constant([1, 0])),
-        #                      lambda: min_idx_reshaped)
-        # left_idx = tf.cond(min_idx_reshaped[1] > tf.constant(0), lambda: tf.add(min_idx_reshaped, tf.constant([0, -1])),
-        #                    lambda: min_idx_reshaped)
-        # right_idx = tf.cond(min_idx_reshaped[1] < self.som_dim[0],
-        #                     lambda: tf.add(min_idx_reshaped, tf.constant([0, 1])),
-        #                     lambda: min_idx_reshaped)
-        #
-        # som_reshaped = tf.reshape(self.codebook, [self.som_dim[0], self.som_dim[1], self.latent_dim])
-        #
-        # nearest_neuron = tf.gather_nd(som_reshaped, min_idx_reshaped)
-        # up_nearest_neuron = tf.gather_nd(som_reshaped, up_idx)
-        # bottom_nearest_neuron = tf.gather_nd(som_reshaped, bottom_idx)
-        # left_nearest_neuron = tf.gather_nd(som_reshaped, left_idx)
-        # right_nearest_neuron = tf.gather_nd(som_reshaped, right_idx)
+    def BMU(self):
+        min_idx = self.k_index
+        nearest_neuron = tf.gather(params=self.codebook, indices=min_idx)
+        return nearest_neuron
 
-        k_1 = min_idx // self.som_dim[0]
-        k_2 = min_idx % self.som_dim[1]
-        k_stacked = tf.stack([k_1, k_2], axis=1)
+    # @lazy_scope
+    # def neighbors(self):
+    #     min_idx = self.k_index
+    #     nearest_neuron = tf.gather(params=self.codebook, indices=min_idx)
+    #     up_nearest_neuron = tf.gather(params=self.codebook,  indices=self.select_up)
+    #     bottom_nearest_neuron = tf.gather(params=self.codebook,  indices=self.select_bottom)
+    #     left_nearest_neuron = tf.gather(params=self.codebook,  indices=self.select_left)
+    #     right_nearest_neuron = tf.gather(params=self.codebook,  indices=self.select_right)
+    #     # k_1 = min_idx // self.som_dim[0]
+    #     # k_2 = min_idx % self.som_dim[1]
+    #     # k_stacked = tf.stack([k_1, k_2], axis=1)
 
-        k1_not_top = tf.less(k_1, tf.constant(self.som_dim[0] - 1, dtype=tf.int64))
-        k1_not_bottom = tf.greater(k_1, tf.constant(0, dtype=tf.int64))
-        k2_not_right = tf.less(k_2, tf.constant(self.som_dim[1] - 1, dtype=tf.int64))
-        k2_not_left = tf.greater(k_2, tf.constant(0, dtype=tf.int64))
+    #     # k1_not_top = tf.less(k_1, tf.constant(self.som_dim[0] - 1, dtype=tf.int64))
+    #     # k1_not_bottom = tf.greater(k_1, tf.constant(0, dtype=tf.int64))
+    #     # k2_not_right = tf.less(k_2, tf.constant(self.som_dim[1] - 1, dtype=tf.int64))
+    #     # k2_not_left = tf.greater(k_2, tf.constant(0, dtype=tf.int64))
 
-        k1_up = tf.where(k1_not_top, tf.add(k_1, 1), k_1)
-        k1_down = tf.where(k1_not_bottom, tf.subtract(k_1, 1), k_1)
-        k2_right = tf.where(k2_not_right, tf.add(k_2, 1), k_2)
-        k2_left = tf.where(k2_not_left, tf.subtract(k_2, 1), k_2)
+    #     # k1_up = tf.where(k1_not_top, tf.add(k_1, 1), k_1)
+    #     # k1_down = tf.where(k1_not_bottom, tf.subtract(k_1, 1), k_1)
+    #     # k2_right = tf.where(k2_not_right, tf.add(k_2, 1), k_2)
+    #     # k2_left = tf.where(k2_not_left, tf.subtract(k_2, 1), k_2)
 
-        nearest_neuron = tf.gather_nd(params=self.codebook, indices=k_stacked, batch_dims=self.batch_size)
+    #     # nearest_neuron = tf.gather_nd(params=self.codebook, indices=k_stacked, batch_dims=self.batch_size)
 
-        up_nearest_neuron = tf.where(k1_not_top, tf.gather_nd(self.codebook, tf.stack([k1_up, k_2], axis=1),
-                                                              batch_dims=self.batch_size),
-                                     tf.zeros([self.batch_size, self.latent_dim]))
-        bottom_nearest_neuron = tf.where(k1_not_bottom, tf.gather_nd(self.codebook, tf.stack([k1_down, k_2], axis=1),
-                                                                     batch_dims=self.batch_size),
-                            tf.zeros([self.batch_size, self.latent_dim]))
-        right_nearest_neuron = tf.where(k2_not_right, tf.gather_nd(self.codebook, tf.stack([k_1, k2_right], axis=1),
-                                                                   batch_dims=self.batch_size),
-                             tf.zeros([self.batch_size, self.latent_dim]))
-        left_nearest_neuron = tf.where(k2_not_left, tf.gather_nd(self.codebook, tf.stack([k_1, k2_left], axis=1),
-                                                                 batch_dims=self.batch_size),
-                            tf.zeros([self.batch_size, self.latent_dim]))
+    #     # up_nearest_neuron = tf.where(k1_not_top, tf.gather_nd(self.codebook, tf.stack([k1_up, k_2], axis=1),
+    #     #                                                       batch_dims=self.batch_size),
+    #     #                              tf.zeros([self.batch_size, self.latent_dim]))
+    #     # bottom_nearest_neuron = tf.where(k1_not_bottom, tf.gather_nd(self.codebook, tf.stack([k1_down, k_2], axis=1),
+    #     #                                                              batch_dims=self.batch_size),
+    #     #                     tf.zeros([self.batch_size, self.latent_dim]))
+    #     # right_nearest_neuron = tf.where(k2_not_right, tf.gather_nd(self.codebook, tf.stack([k_1, k2_right], axis=1),
+    #     #                                                            batch_dims=self.batch_size),
+    #     #                      tf.zeros([self.batch_size, self.latent_dim]))
+    #     # left_nearest_neuron = tf.where(k2_not_left, tf.gather_nd(self.codebook, tf.stack([k_1, k2_left], axis=1),
+    #     #                                                          batch_dims=self.batch_size),
+    #     #                     tf.zeros([self.batch_size, self.latent_dim]))
 
-        som_neighbors = tf.stack([nearest_neuron, up_nearest_neuron, bottom_nearest_neuron, left_nearest_neuron,
-                                  right_nearest_neuron], axis=1)
-        return som_neighbors
+    #     som_neighbors = tf.stack([nearest_neuron, up_nearest_neuron, bottom_nearest_neuron, left_nearest_neuron,
+    #                               right_nearest_neuron], axis=1)
+    #     return som_neighbors
+
+    @lazy_scope
+    def loss_bmu(self):
+        loss_som = tf.reduce_mean(
+            tf.squared_difference(self.inputs, self.BMU))
+        tf.compat.v1.summary.scalar("bmu_loss", loss_som)
+        return loss_som
+
+    @lazy_scope
+    def loss_bmu_up(self):
+        bmu = self.k_index
+        _bmu = tf.unravel_index(bmu,[self.som_dim[0], self.som_dim[1]])
+        zeros = tf.zeros_like(_bmu[0])
+        mask = tf.greater(_bmu[0], zeros) # boolean tensor, mask[i] = True iff x[i] > 0
+        _bmu_movable_sx = tf.boolean_mask(_bmu[0], mask)
+        _bmu_movable_dx = tf.boolean_mask(_bmu[1], mask)
+        _bmu_raveled = tf.multiply(tf.add(_bmu_movable_sx, -1),self.som_dim[0]) + (_bmu_movable_dx % self.som_dim[1])
+        _inputs_idx = tf.boolean_mask(self.inputs, mask)
+        current_loss = tf.reduce_mean(
+            tf.squared_difference(_inputs_idx, tf.gather(self.codebook, _bmu_raveled)))
+        return current_loss
+        # return _bmu_raveled
+
+
+    @lazy_scope
+    def loss_bmu_bottom(self):
+        bmu = self.k_index
+        _bmu = tf.unravel_index(bmu,[self.som_dim[0], self.som_dim[1]])
+        bottomers = tf.multiply(tf.ones_like(_bmu[0]), self.som_dim[1])
+        mask = tf.less(_bmu[0], bottomers) # boolean tensor, mask[i] = True iff x[i] > 0
+        _bmu_movable_sx = tf.boolean_mask(_bmu[0], mask)
+        _bmu_movable_dx = tf.boolean_mask(_bmu[1], mask)
+        _bmu_raveled = tf.multiply(tf.add(_bmu_movable_sx, 1),self.som_dim[0]) + (_bmu_movable_dx % self.som_dim[1])
+        _inputs_idx = tf.boolean_mask(self.inputs, mask)
+        current_loss = tf.reduce_mean(
+            tf.squared_difference(_inputs_idx, tf.gather(self.codebook, _bmu_raveled)))
+        return current_loss
+
+    @lazy_scope
+    def loss_bmu_left(self):
+        bmu = self.k_index
+        _bmu = tf.unravel_index(bmu,[self.som_dim[0], self.som_dim[1]])
+        zeros = tf.zeros_like(_bmu[1])
+        mask = tf.greater(_bmu[1], zeros) # boolean tensor, mask[i] = True iff x[i] > 0
+        _bmu_movable_sx = tf.boolean_mask(_bmu[0], mask)
+        _bmu_movable_dx = tf.boolean_mask(_bmu[1], mask)
+        _bmu_raveled = tf.multiply(_bmu_movable_sx,self.som_dim[0]) + (tf.add(_bmu_movable_dx, -1) % self.som_dim[1])
+        _inputs_idx = tf.boolean_mask(self.inputs, mask)
+        current_loss = tf.reduce_mean(
+            tf.squared_difference(_inputs_idx, tf.gather(self.codebook, _bmu_raveled)))
+        return current_loss
+
+
+    @lazy_scope
+    def loss_bmu_right(self):
+        bmu = self.k_index
+        _bmu = tf.unravel_index(bmu,[self.som_dim[0], self.som_dim[1]])
+        righters = tf.multiply(tf.ones_like(_bmu[1]), self.som_dim[1])
+        mask = tf.less(_bmu[1], righters) # boolean tensor, mask[i] = True iff x[i] > 0
+        _bmu_movable_sx = tf.boolean_mask(_bmu[0], mask)
+        _bmu_movable_dx = tf.boolean_mask(_bmu[1], mask)
+        _bmu_raveled = tf.multiply(_bmu_movable_sx,self.som_dim[0]) + (tf.add(_bmu_movable_dx, 1) % self.som_dim[1])
+        _inputs_idx = tf.boolean_mask(self.inputs, mask)
+        current_loss = tf.reduce_mean(
+            tf.squared_difference(_inputs_idx, tf.gather(self.codebook, _bmu_raveled)))
+        return current_loss
 
     @lazy_scope
     def loss(self):
-        loss_som = tf.reduce_mean(
-            tf.squared_difference(tf.expand_dims(tf.stop_gradient(self.inputs), axis=2), self.neighbors))
-        tf.summary.scalar("loss_som", loss_som)
-        return loss_som
+        loss = (self.loss_bmu + self.alpha*self.loss_bmu_up + self.alpha*self.loss_bmu_bottom
+                + self.alpha*self.loss_bmu_left + self.alpha*self.loss_bmu_right )
+        tf.compat.v1.summary.scalar("loss", loss)
+        return loss
 
     @lazy_scope
     def optimize(self):
         """Optimizes the model's loss using Adam with exponential learning rate decay."""
-        lr_decay = tf.train.exponential_decay(self.learning_rate, self.global_step, self.decay_steps, self.decay_factor, staircase=True)
-        optimizer = tf.train.AdamOptimizer(lr_decay)
+        lr_decay = tf.compat.v1.train.exponential_decay(self.learning_rate, self.global_step, self.decay_steps, self.decay_factor, staircase=True)
+        optimizer = tf.compat.v1.train.AdamOptimizer(lr_decay)
         train_step = optimizer.minimize(self.loss, global_step=self.global_step)
         return train_step
+
+    # @lazy_scope
+    # def plot_neurons(self):
+    #     codebook = np.reshape(self.codebook.eval(), [self.som_dim[0], self.som_dim[1],28,28])
+    #     fig = plt.figure(figsize=(20,20))
+    #     fig.subplots_adjust(hspace=0.2, wspace=0.05)
+    #     k = 0
+    #     for i, j in itertools.product(range(self.som_dim[0]), range(self.som_dim[1])):
+    #         ax = fig.add_subplot(self.som_dim[0], self.som_dim[1], k + 1)
+    #         ax.matshow(codebook[i,j])
+    #         ax.get_xaxis().set_visible(False)
+    #         ax.get_yaxis().set_visible(False)
+    #         k += 1
+    #     plt.show()
+    #     buf = io.BytesIO()
+    #     plt.savefig(buf, format='png')
+    #     plt.savefig('neurons.png')
+    #     # Closing the figure prevents it from being displayed directly inside
+    #     # the notebook.
+    #     plt.close(fig)
+    #     buf.seek(0)
+    #     # Convert PNG buffer to TF image
+    #     image = tf.image.decode_png(buf.getvalue(), channels=1)
+    #     # Add the batch dimension
+    #     image = tf.expand_dims(image, 0)
+    #     tf.compat.v1.summary.image("SOM Neurons", image)
--- som_ae/som_train.py
+++ som_ae/som_train.py
@@ -5,13 +5,19 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from tensorflow.examples.tutorials.mnist import input_data
 from tqdm import tqdm
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import io
+import itertools
 
 
 mnist = input_data.read_data_sets(f"../data/MNIST_data")
 
 data_train = np.reshape(mnist.train.images, [-1, 28*28])
-data_val = data_train[45000:]
-data_train = data_train[:45000]
+data_val = np.reshape(mnist.test.images, [-1, 28*28])
+# data_train = data_train[:45000]
+print('TRAIN',data_train.shape,'VAL', data_val.shape)
 
 
 def get_data_generator(train_data, validation_data):
@@ -35,7 +41,7 @@ def get_data_generator(train_data, validation_data):
 
 
 def train_model(model=None, x=None,  lr_val=None, num_epochs=None, patience=None, batch_size=None,
-                learning_rate=None, interactive=True, generator=None):
+                learning_rate=None, interactive=True, generator=None, sess = None):
     """Trains the SOM-VAE model.
 
     Args:
@@ -52,70 +58,102 @@ def train_model(model=None, x=None,  lr_val=None, num_epochs=None, patience=None
             progress bar for training.
         generator (generator): Generator for the data batches.
     """
+
+    
+
+
+
     train_gen = generator("train", batch_size)
     val_gen = generator("val", batch_size)
 
+
+    train_writer = tf.summary.FileWriter('../logs' + '/train', sess.graph)
     num_batches = len(data_train) // batch_size
+    summaries = tf.compat.v1.summary.merge_all()
+    sess.run(tf.global_variables_initializer())
+    patience_count = 0
+    test_losses = []
+    print("Training...")
+    train_step_SOM = model.optimize
+    pbar = tqdm(total= num_epochs * num_batches)
+    for epoch in range(num_epochs):
+        batch_val = next(val_gen)
+        test_loss, summary = sess.run([model.loss, summaries], feed_dict={x: batch_val})
+        test_losses.append(test_loss)
+        if test_losses[-1] == min(test_losses):
+            patience_count = 0
+        else:
+            patience_count += 1
+        if patience_count >= patience:
+            break
+        for i in range(num_batches):
+            batch_data = next(train_gen)
+            if i % 100 == 0:
+                train_loss, summary, global_step = sess.run([model.loss, summaries, model.global_step], feed_dict={x: batch_data})                
+                train_writer.add_summary(summary, global_step=global_step)
+            
+            train_step_SOM.run(feed_dict={x: batch_data, lr_val: learning_rate})
+            pbar.set_postfix(epoch=epoch, train_loss=train_loss, test_loss=test_loss, refresh=False)
+            pbar.update(1)
 
-    summaries = tf.summary.merge_all()
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    with tf.Session(config=config) as sess:
-        sess.run(tf.global_variables_initializer())
-        patience_count = 0
-        test_losses = []
-        print("Training...")
-        train_step_SOMVAE = model.optimize
-        if interactive:
-            pbar = tqdm(total= num_epochs * num_batches)
-        for epoch in range(num_epochs):
-            batch_val = next(val_gen)
-            test_loss, summary = sess.run([model.loss, summaries], feed_dict={x: batch_val})
-            test_losses.append(test_loss)
-            if test_losses[-1] == min(test_losses):
-                patience_count = 0
-            else:
-                patience_count += 1
-            if patience_count >= patience:
-                break
-            for i in range(num_batches):
-                batch_data = next(train_gen)
-                if i % 100 == 0:
-                    train_loss, summary = sess.run([model.loss, summaries], feed_dict={x: batch_data})
-                train_step_SOMVAE.run(feed_dict={x: batch_data, lr_val: learning_rate})
-                if interactive:
-                    pbar.set_postfix(epoch=epoch, train_loss=train_loss, test_loss=test_loss, refresh=False)
-                    pbar.update(1)
-            if interactive:
-                pbar.close()
 
+        # sess.run(model.plot_neurons)
+        # summary, global_step = sess.run([model.plot_neurons, summaries, model.global_step])
+        # train_writer.add_summary(summary, global_step=global_step)
+        codebook = np.reshape(model.codebook.eval(), [model.som_dim[0], model.som_dim[1],28,28])
+        fig = plt.figure(figsize=(20,20))
+        fig.subplots_adjust(hspace=0.2, wspace=0.05)
+        k = 0
+        for i, j in itertools.product(range(model.som_dim[0]), range(model.som_dim[1])):
+            ax = fig.add_subplot(model.som_dim[0], model.som_dim[1], k + 1)
+            ax.matshow(codebook[i,j])
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+            k += 1
+        plt.show()
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        plt.savefig('neurons.png')
+        # Closing the figure prevents it from being displayed directly inside
+        # the notebook.
+        plt.close(fig)
+        # buf.seek(0)
+        # # Convert PNG buffer to TF image
+        # image = tf.image.decode_png(buf.getvalue(), channels=1)
+        # # Add the batch dimension
+        # image = tf.expand_dims(image, 0)
+        # tf.compat.v1.summary.image("SOM Neurons", image)
 
 if __name__ == '__main__':
     input_length = 28
     input_channels = 28
-    x = tf.placeholder(tf.float32, shape=[None, 28*28])
-    learning_rate = 0.0005
+    learning_rate = 0.001
     data_generator = get_data_generator(data_train, data_val)
-    som_dim = np.array([4, 4], dtype='int32')
+    som_dim = np.array([8, 8], dtype='int32')
     latent_dim = 28*28
-    alpha = 1.0
+    alpha = 0.95
     beta = 0.9
     gamma = 1.8
     tau = 1.4
     decay_factor = 0.9
-    decay_steps = 1000
+    decay_steps = 500
     patience = 100
-    num_epochs = 20
-    batch_size = 128
+    num_epochs = 50
+    batch_size = 512
     lr_val = tf.placeholder_with_default(learning_rate, [])
+    x = tf.placeholder(tf.float32, shape=[batch_size, 28*28])
 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        model = somTF(inputs=x, latent_dim=latent_dim, som_dim=som_dim, learning_rate=lr_val,
+                    decay_factor=decay_factor, decay_steps=decay_steps, batch_size=batch_size, alpha=alpha)
+        train_model(model=model, x=x, lr_val=lr_val, learning_rate=learning_rate, interactive=True,
+                    generator=data_generator, patience=patience, num_epochs=num_epochs, batch_size= batch_size, sess=sess)
+        
+        print('Finish')
+    print('Closing session')
 
-    model = somTF(inputs=x, latent_dim=latent_dim, som_dim=som_dim, learning_rate=lr_val,
-                  decay_factor=decay_factor, decay_steps=decay_steps, batch_size=batch_size)
-    train_model(model=model, x=x, lr_val=lr_val, learning_rate=learning_rate, interactive=True,
-                generator=data_generator, patience=patience, num_epochs=num_epochs, batch_size= batch_size)
-    codebook = tf.reshape(model.codebook, [28, 28])
-    print('Finish')
 
 
 
